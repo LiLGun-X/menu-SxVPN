45598b1d265d7db23d106191ce0062b7f2c686fe
LiLGun-X <91993374+LiLGun-X@users.noreply.github.com>
2021-10-11 22:06:46 +0700

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -4,8 +4,9 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 clear
+echo -e "***********************************" | lolcat
 read -p "โปรดใส่ชื่อที่ต้องการลบ : " Pengguna
-
+echo -e "***********************************" | lolcat
 if getent passwd $Pengguna > /dev/null 2>&1; then
         userdel $Pengguna
         echo -e "$Pengguna ถูกลบออกจากระบบแล้ว."
