275b6bb7cfd5b1ab46436f1d4b862e0c5c60f6ec
LiLGun-X <91993374+LiLGun-X@users.noreply.github.com>
2021-10-11 22:03:05 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -10,6 +10,9 @@ if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
+echo -e "***********************************" | lolcat
+echo -e "             สร้างชื่อผู้ใช่"
+echo -e "***********************************" | lolcat
 fi
 read -p "ชื่อ : " Login
 read -p "รหัสผ่าน : " Pass
@@ -36,21 +39,21 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "**********************************************"
+echo -e "**********************************************" | lolcat
 echo -e "                 ข้อมูลบัญชี"
 echo -e "ไอพี            :  $IP"
 echo -e "โฮสต์           :  $domain" 
 echo -e "ชื่อ​             :  $Login"
 echo -e "รหัสผ่าน​         :  $Pass"
 echo -e "วันหมดอายุ       :  $exp"
-echo -e "**********************************************"
+echo -e "**********************************************" | lolcat
 echo -e "                SSH SSL" 
 echo -e "OpenSSH        :  22"
 echo -e "Dropbear       :  109,143"
 echo -e "SSL/TLS        : $ssl"
 echo -e "Port Squid     : $sqd"
 echo -e "Badvpn         :  7100-7300"
-echo -e "**********************************************"
+echo -e "**********************************************" | lolcat
 echo -e "                OVPN"
 echo -e "OVPN TCP       :  $ovpn"
 echo -e "OVPN UDP       :  $ovpn2"
@@ -58,5 +61,5 @@ echo -e "OVPN SSL       :  442"
 echo -e "TCP conf ลิงค์​   :  http://$IP:81/TCP.ovpn"
 echo -e "UDP conf ลิงค์​   :  http://$IP:81/UDP.ovpn"
 echo -e "SSL conf ลิงค์​   :  http://$IP:81/SSL.ovpn"
-echo -e "**********************************************"
-echo -e "สคริปโดยเอเจ" 
+echo -e "**********************************************" | lolcat
+echo -e "By Lil Gun-X" 
